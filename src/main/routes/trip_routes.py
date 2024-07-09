ea221c4e82a037ce33672598f592b031fc890437
renato-avellar <o.onizuka@icloud.com>
2024-07-09 17:59:06 -0300

    config: adding firts test route to project

---
+++ src/main/routes/trip_routes.py
@@ -0,0 +1,7 @@
+from flask import jsonify, Blueprint
+
+trip_routes_bp = Blueprint("trip_routes", __name__)
+
+@trip_routes_bp.route("/trips", methods=["POST"])
+def create_trip():
+    return jsonify({"hello": "world"})
